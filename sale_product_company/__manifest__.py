f08222f96c91ba405a62cc5b1d6dbd2539ec3955
Telmo Santos <telmo.santos@camptocamp.com>
2024-02-09 09:54:28 +0100

    [ADD] sale_product_company

---
+++ sale_product_company/__manifest__.py
@@ -0,0 +1,20 @@
+# Copyright 2024 Camptocamp SA
+# License AGPL-3.0 or later (https://www.gnu.org/licenses/agpl)
+{
+    "name": "sale product company",
+    "summary": "Set selling companies on product",
+    "version": "16.0.1.0.0",
+    "development_status": "Alpha",
+    "category": "Sale Management",
+    "website": "https://github.com/OCA/multi-company",
+    "author": "Camptocamp, Odoo Community Association (OCA)",
+    "license": "AGPL-3",
+    "application": False,
+    "installable": True,
+    "data": [
+        "views/product_template_view.xml",
+    ],
+    "depends": [
+        "sale",
+    ],
+}
